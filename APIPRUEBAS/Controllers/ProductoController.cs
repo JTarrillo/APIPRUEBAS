a2ef2107d9644f267a35c5f0900e6067a0f63823
Jose <desarrollo.lx@logixtal.com>
2022-12-30 20:52:45 -0500

    Usando HTTPGET

--- APIPRUEBAS/Controllers/ProductoController.cs
+++ APIPRUEBAS/Controllers/ProductoController.cs
@@ -19,5 +19,29 @@ namespace APIPRUEBAS.Controllers
         {
             _dbcontext = _context;
         }
+
+
+        [HttpGet]
+        [Route("Lista")]
+
+        public IActionResult Lista()
+        {
+
+            List<Producto> lista = new List<Producto>();
+
+            try
+            {
+                lista = _dbcontext.Productos.ToList();
+
+
+                return StatusCode(StatusCodes.Status200OK, new {mensaje = "ok", response = lista});
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message, response = lista });
+                throw;
+            }
+        }
+
     }
 }
