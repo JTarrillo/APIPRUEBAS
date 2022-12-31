0c497a99f0c32f54776b79e9212ec1ba29db7ff3
Jose <desarrollo.lx@logixtal.com>
2022-12-31 16:34:06 -0500

    Crud api

--- APIPRUEBAS/Controllers/ProductoController.cs
+++ APIPRUEBAS/Controllers/ProductoController.cs
@@ -88,5 +88,72 @@ namespace APIPRUEBAS.Controllers
                 throw;
             }
         }
+
+        [HttpPut]
+        [Route("Editar")]
+
+        public IActionResult Editar([FromBody] Producto objeto)
+        {
+
+            Producto oProducto = _dbcontext.Productos.Find(objeto.IdProducto);
+            if (oProducto == null)
+            {
+                return BadRequest("Producto no encontrado");
+            }
+
+
+
+            try
+            {
+                oProducto.CodigoBarra = objeto.CodigoBarra is null ? oProducto.CodigoBarra : objeto.CodigoBarra;
+                oProducto.Descripcion = objeto.Descripcion is null ? oProducto.Descripcion : objeto.Descripcion;
+                oProducto.Marca = objeto.Marca is null ? oProducto.Marca : objeto.Marca;
+                oProducto.IdCategoria = objeto.IdCategoria is null ? oProducto.IdCategoria : objeto.IdCategoria;
+                oProducto.Precio = objeto.Precio is null ? oProducto.Precio : objeto.Precio;
+               
+                _dbcontext.Productos.Update(oProducto);
+                _dbcontext.SaveChanges();
+
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message });
+                throw;
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("Eliminar/{idProducto:int}")]
+
+        public IActionResult Eliminar(int idProducto)
+        {
+
+            Producto oProducto = _dbcontext.Productos.Find(idProducto);
+            if (oProducto == null)
+            {
+                return BadRequest("Producto no encontrado");
+            }
+
+
+
+            try
+            {
+                
+
+                _dbcontext.Productos.Remove(oProducto);
+                _dbcontext.SaveChanges();
+
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message });
+                throw;
+            }
+        }
     }
 }
